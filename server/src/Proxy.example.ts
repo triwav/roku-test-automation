c00b03d4e59444f6924ce6b28747489b368a73c4
Charlie Abbott <charlie.abbott@gmail.com>
2020-04-07 11:08:58 +0100

    Setting up an example of using the `http-proxy-middleware` npm package and Express JS

--- server/src/Proxy.example.ts
+++ server/src/Proxy.example.ts
@@ -8,6 +8,7 @@ const options: Options = {
     changeOrigin: true,             // Needed for virtual hosted sites - may be able to remove
     followRedirects: true,
     onProxyReq: onProxyRequest,
+    //FIXME: this is not fixing the missing image errors within the app - was seeing cached images that appeared to make this fix good
     router: {
         'gn-imgx.fubo.tv': 'https://gn-imgx.fubo.tv',
         'imgx.fubo.tv': 'https://imgx.fubo.tv',
