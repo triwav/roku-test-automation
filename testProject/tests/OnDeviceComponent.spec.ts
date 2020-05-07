060729026e6670ff7e21cd7abfc1012beb53fffc
Brian Leighty <bleighty@fubo.tv>
2020-05-07 15:33:13 -0400

    Switch getValueAtKeyPath to return an object, remove samples folder

--- testProject/tests/OnDeviceComponent.spec.ts
+++ testProject/tests/OnDeviceComponent.spec.ts
@@ -8,27 +8,27 @@ const {device, odc, ecp} = utils.setupFromConfigFile();
 describe('OnDeviceComponent', function () {
 	describe('getValueAtKeyPath', function () {
 		it('should work with findnode', async () => {
-			const value = await odc.getValueAtKeyPath('scene', 'subchild3');
+			const {value} = await odc.getValueAtKeyPath('scene', 'subchild3');
 			expect(value.id).to.eq('subchild3');
 		});
 
 		it('should not find a child if it is not beneath the parent node', async () => {
-			const value = await odc.getValueAtKeyPath('scene', 'subchild3.testTarget');
+			const {value} = await odc.getValueAtKeyPath('scene', 'subchild3.testTarget');
 			expect(value.id).to.be.undefined;
 		});
 
 		it('should work with findNode.getChild', async () => {
-			const value = await odc.getValueAtKeyPath('scene', 'testTarget.0');
+			const {value} = await odc.getValueAtKeyPath('scene', 'testTarget.0');
 			expect(value.id).to.eq('child1');
 		});
 
 		it('should work with findNode.getChild.getChild', async () => {
-			const value = await odc.getValueAtKeyPath('scene', 'testTarget.1.1');
+			const {value} = await odc.getValueAtKeyPath('scene', 'testTarget.1.1');
 			expect(value.id).to.eq('subchild2');
 		});
 
 		it('should work with findNode.getChild.findNode', async () => {
-			const value = await odc.getValueAtKeyPath('scene', 'testTarget.1.subchild1');
+			const {value} = await odc.getValueAtKeyPath('scene', 'testTarget.1.subchild1');
 			expect(value.id).to.eq('subchild1');
 		});
 
@@ -42,17 +42,19 @@ describe('OnDeviceComponent', function () {
 		});
 
 		it('should be able to get a value on a valid field', async () => {
-			const value = await odc.getValueAtKeyPath('global', 'authManager.isLoggedIn');
+			const {value} = await odc.getValueAtKeyPath('global', 'authManager.isLoggedIn');
 			expect(value).to.be.false;
 		});
 	});
 
 	describe('setValueAtKeyPath', function () {
-		async function setAndVerifyValue(base: KeyPathBaseTypes, keyPath: string, value: any, startingValue: any) {
-			expect(await odc.getValueAtKeyPath(base, keyPath)).to.equal(startingValue, `${base}.${keyPath} did not match expected value before set`);
+		async function setAndVerifyValue(base: KeyPathBaseTypes, keyPath: string, value: any, expectedStartingValue: any) {
+			const {value: startingValue} = await odc.getValueAtKeyPath(base, keyPath);
+			expect(startingValue).to.equal(expectedStartingValue, `${base}.${keyPath} did not match expected value before set`);
 			const result = await odc.setValueAtKeyPath(base, keyPath, value);
 			expect(result.success).to.be.true;
-			expect(await odc.getValueAtKeyPath(base, keyPath)).to.equal(value, `${base}.${keyPath} did not match expected value after set`);
+			const {value: endingValue} = await odc.getValueAtKeyPath(base, keyPath);
+			expect(endingValue).to.equal(value, `${base}.${keyPath} did not match expected value after set`);
 		}
 
 		it('should be able to set a key on global', async () => {
