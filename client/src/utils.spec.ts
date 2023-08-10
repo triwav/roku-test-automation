2f73f352e8a07c2f600b334e834274aee5a5763b
Brian Leighty <bleighty@tubi.tv>
2023-08-10 17:54:01 -0400

    Add support for extending rta-config.json

---
+++ client/src/utils.spec.ts
@@ -0,0 +1,16 @@
+import * as chai from 'chai';
+const expect = chai.expect;
+
+
+import {utils} from './utils';
+
+describe('utils', function () {
+	describe('getConfigFromConfigFile', function () {
+		it('allows us to extend the config and have the correct values', () => {
+			const config = utils.getConfigFromConfigFile('rta-config.json');
+
+			expect(config.RokuDevice.devices.length).to.be.greaterThan(0);
+			expect(config.OnDeviceComponent?.helperInjection?.componentPaths).to.include('components/MainScene.xml');
+		});
+	});
+});
